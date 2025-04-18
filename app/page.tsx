cf921224748c623a4c7935661b4b145b24a7008e
ankitmrmishra <ankitmrmishra1118@gmail.com>
2025-04-18 17:33:34 +0530

    added the showcase component in the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/Landing/Hero";
+import Showcase from "@/components/Landing/Showcase";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
+      <Showcase />
     </div>
   );
 }
