7cadb98edaa1a31ff1f945c18f70a532481a74e5
ankitmrmishra <ankitmrmishra1118@gmail.com>
2025-06-01 16:06:46 +0530

    added the email verifictaion logic

--- app/api/auth/verifyemail/route.ts
+++ app/api/auth/verifyemail/route.ts
@@ -1,14 +1,47 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/db/prisma";
+export async function GET(req: NextRequest) {
   try {
-  } catch (error) {
-    console.error("Error Creating user:", error);
-    return NextResponse.json(
-      {
-        message: "Failed to create user, please try again",
-        error: error instanceof Error ? error.message : String(error),
+    const queryParams = req.nextUrl.searchParams;
+    const token = queryParams.get("token");
+    const email = queryParams.get("email");
+    if (!token || !email) {
+      return NextResponse.json({ error: "Invalid query" }, { status: 400 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
       },
+    });
+    if (!user) {
+      return NextResponse.json({ error: "User Not found" }, { status: 400 });
+    }
+
+    const TokenexpiryTime = user.verificationTime;
+    if (TokenexpiryTime) {
+      if (TokenexpiryTime?.getTime() < Date.now()) {
+        return NextResponse.json({ error: "Token Expired" }, { status: 400 });
+      } else {
+        await prisma.user.update({
+          where: {
+            email: email,
+          },
+          data: {
+            isemailverifed: true,
+            verificationToken: null,
+            verificationTime: null,
+          },
+        });
+
+        return NextResponse.redirect(new URL("/login", req.url));
+      }
+    }
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      { error: "Email Verification Failed", details: error },
       { status: 500 }
     );
   }
